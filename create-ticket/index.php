08314ac812fb9a7eac81d00f38c5c5b124127fa9
wjbaker101 <willbaker10198@gmail.com>
2018-02-09 11:54:23 +0000

    Remove PHP errors

--- create-ticket/index.php
+++ create-ticket/index.php
@@ -114,7 +114,7 @@ include('create.php');
                         <div class="content-section padding-small">
                             <h2>Ticket Details</h2>
                             <h4><abbr title="A short description to help distinguish between tickets.">Short Summary:</abbr></h4>
-                            <input class="summary-input" type="text" name="summary" value="<?php echo htmlspecialchars($_POST['summary']); ?>" autofocus>
+                            <input class="summary-input" type="text" name="summary" value="<?php if (isset($_POST['summary'])) echo htmlspecialchars($_POST['summary']); ?>" autofocus>
                             <?= $messages->summary ?>
                             <h4><abbr title="How should this ticket be treated relative to others.">Priority:</abbr></h4>
                             <p>
@@ -151,10 +151,10 @@ include('create.php');
                             <div class="column-container">
                                 <div class="column l6 s12 v-content-section">
                                     <h4>Employee ID</h4>
-                                    <input class="caller-id-input" type="text" name="caller-id" value="<?php echo htmlspecialchars($_POST['caller-id']); ?>">
+                                    <input class="caller-id-input" type="text" name="caller-id" value="<?php if (isset($_POST['caller-id'])) echo htmlspecialchars($_POST['caller-id']); ?>">
                                     <?= $messages->callerID ?>
                                     <h4>Telephone Number</h4>
-                                    <input class="telephone-input" type="text" name="telephone-number" value="<?php echo htmlspecialchars($_POST['telephone-number']); ?>">
+                                    <input class="telephone-input" type="text" name="telephone-number" value="<?php if (isset($_POST['telephone-number'])) echo htmlspecialchars($_POST['telephone-number']); ?>">
                                 </div>
                                 <div class="column l6 s12 v-content-section">
                                     <h4>Search Employee Name</h4>
@@ -169,7 +169,7 @@ include('create.php');
                     <div class="bg-white shadow padding-small section">
                         <div class="content-section padding-small">
                             <h2><abbr title="A more detailed explanation about the problem.">Full Ticket Details</abbr></h2>
-                            <textarea class="description-input" name="description" placeholder="A more detailed explanation about the problem."><?php echo htmlspecialchars($_POST['description']); ?></textarea>
+                            <textarea class="description-input" name="description" placeholder="A more detailed explanation about the problem."><?php if (isset($_POST['description'])) echo htmlspecialchars($_POST['description']); ?></textarea>
                             <?= $messages->description ?>
                         </div>
                     </div>
@@ -177,7 +177,7 @@ include('create.php');
                         <div class="content-section padding-small">
                             <h2>Problem Information</h2>
                             <h4><abbr title="The type of problem this ticket is categorised as.">Problem Type</abbr></h4>
-                            <!--<input class="problem-type-input" type="text" name="problem-type" value="<?php echo htmlspecialchars($_POST['problem-type']); ?>">-->
+                            <!--<input class="problem-type-input" type="text" name="problem-type" value="<?php if (isset($_POST['problem-type'])) echo htmlspecialchars($_POST['problem-type']); ?>">-->
                             <select>
                                 <option>Network</option>
                                 <option>Hardware</option>
@@ -185,29 +185,29 @@ include('create.php');
                             </select>
                             <?= $messages->problemType ?>
                             <h4><abbr title="Hardware serial ID related to the problem.">Hardware Serial ID</abbr></h4>
-                            <input class="hardware-serial-id-input" type="text" name="hardware-serial-id" placeholder="7EIQ-72IU-2YNV-3L4Y" value="<?php echo htmlspecialchars($_POST['hardware-serial-id']); ?>">
+                            <input class="hardware-serial-id-input" type="text" name="hardware-serial-id" placeholder="7EIQ-72IU-2YNV-3L4Y" value="<?php if (isset($_POST['hardware-serial-id'])) echo htmlspecialchars($_POST['hardware-serial-id']); ?>">
                             <?= $messages->hardwareSerialID ?>
                             <div class="column-container">
                                 <div class="column l6 s12">
                                     <h4><abbr title="The name of the operating system being used.">Device Operating System</abbr></h4>
-                                    <input class="os-input" type="text" name="operating-system" placeholder="Windows 10" value="<?php echo htmlspecialchars($_POST['operating-system']); ?>">
+                                    <input class="os-input" type="text" name="operating-system" placeholder="Windows 10" value="<?php if (isset($_POST['operating-system'])) echo htmlspecialchars($_POST['operating-system']); ?>">
                                     <?= $messages->operatingSystem ?>
                                 </div>
                                 <div class="column l6 s12">
                                     <h4><abbr title="The version number of the operating system.">Operating System Version</abbr></h4>
-                                    <input class="os-version-input" type="text" name="os-version" placeholder="5.7.1.0" value="<?php echo htmlspecialchars($_POST['os-version']); ?>">
+                                    <input class="os-version-input" type="text" name="os-version" placeholder="5.7.1.0" value="<?php if (isset($_POST['os-version'])) echo htmlspecialchars($_POST['os-version']); ?>">
                                     <?= $messages->operatingSystem ?>
                                 </div>
                             </div>
                             <div class="column-container">
                                 <div class="column l6 s12">
                                     <h4><abbr title="The name of the software related to the problem.">Software Name</abbr></h4>
-                                    <input class="software-input" type="text" name="software" placeholder="Google Chrome" value="<?php echo htmlspecialchars($_POST['software']); ?>">
+                                    <input class="software-input" type="text" name="software" placeholder="Google Chrome" value="<?php if (isset($_POST['software'])) echo htmlspecialchars($_POST['software']); ?>">
                                     <?= $messages->operatingSystem ?>
                                 </div>
                                 <div class="column l6 s12">
                                     <h4><abbr title="The version number of the software.">Software Version</abbr></h4>
-                                    <input class="software-version-input" type="text" name="software-version" placeholder="12.3.6.1" value="<?php echo htmlspecialchars($_POST['software-version']); ?>">
+                                    <input class="software-version-input" type="text" name="software-version" placeholder="12.3.6.1" value="<?php if (isset($_POST['software-version'])) echo htmlspecialchars($_POST['software-version']); ?>">
                                     <?= $messages->operatingSystem ?>
                                 </div>
                             </div>
@@ -224,7 +224,7 @@ include('create.php');
                             </div>
                             <div class="specialist-info-details section-details <?php echo isset($_POST['assign-specialist']) ? '' : 'visible' ?>">
                                 <h4><abbr title="The Employee ID of the specialist.">Specialist ID</abbr></h4>
-                                <input class="specialist-id-input" type="text" name="specialist-id" value="<?php echo htmlspecialchars($_POST['specialist-id']); ?>">
+                                <input class="specialist-id-input" type="text" name="specialist-id" value="<?php if (isset($_POST['specialist-id'])) echo htmlspecialchars($_POST['specialist-id']); ?>">
                                 <?= $messages->specialist ?>
                                 <h4><abbr title="First and surname of the specialist for searching.">Specialist Name</abbr></h4>
                                 <p>Search for the specialist's ID:</p>
@@ -241,13 +241,13 @@ include('create.php');
                             <div class="cell-row">
                                 <h2 class="cell l6 cell-middle">Immediately Close Ticket</h2>
                                 <div class="cell l6 cell-middle text-right">
-                                    <input class="close-ticket-enabled" type="checkbox" name="resolution" value="checked" <?php echo isset($_POST['resolution']) ? 'checked' : '' ?>>
+                                    <input class="close-ticket-enabled" type="checkbox" name="resolution" value="checked" <?php if (isset($_POST['resolution'])) echo isset($_POST['resolution']) ? 'checked' : '' ?>>
                                     <span>Enable</span>
                                 </div>
                             </div>
                             <div class="close-ticket-details section-details <?php echo isset($_POST['resolution']) && strlen($_POST['resolution']) > 0 ? 'visible' : '' ?>">
                                 <h4><abbr title="A description of how the problem was resolved.">Resolution Description</abbr></h4>
-                                <textarea class="description-input" name="resolution-description"><?php echo htmlspecialchars($_POST['resolution-description']); ?></textarea>
+                                <textarea class="description-input" name="resolution-description"><?php if (isset($_POST['resolution-description'])) echo htmlspecialchars($_POST['resolution-description']); ?></textarea>
                                 <?= $messages->resolutionDescription ?>
                             </div>
                         </div>
