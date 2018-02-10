503147d78a003c4b61bd68b10d1e3f5a8df96d33
wjbaker101 <willbaker10198@gmail.com>
2018-02-10 21:56:26 +0000

    Add call logs to ticket information page

--- ticket/index.php
+++ ticket/index.php
@@ -176,8 +176,15 @@ if (!isset($ticket))
                 </div>
                 <div class="bg-white shadow section">
                     <div class="content-section padding-small">
-                        <h2>Call Logs</h2>
+                        <h2>Call Logs
+                            <a href="/create-call-log/?id=<?php echo htmlspecialchars($_GET['id']); ?>" title="Create a new call log for this ticket.">
+                                <svg width="17" class="cell-middle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path fill="#000" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"></path>
+                                </svg>
+                            </a>
+                        </h2>
                     </div>
+                    <?php include('get-call-logs.php'); ?>
                 </div>
                 <div class="bg-white shadow section">
                     <div class="content-section padding-small">
