647fe2006fd666cac99cd158c7e494e513d51db5
wjbaker101 <willbaker10198@gmail.com>
2018-02-17 19:07:05 +0000

    Create footer

--- resources/page/components/footer.php
+++ resources/page/components/footer.php
@@ -1,2 +1,14 @@
 <footer>
+    <div class="content">
+        <div class="dynamic-font-container">
+            <p>
+                <strong>Select a font size:</strong>
+                <button class="font-small-button">A</button>
+                <button class="font-large-button">A</button>
+            </p>
+        </div>
+        <p>Team 18 Helpdesk created by Team 18</p>
+        <p>Copyright &copy; Team 18, 2017-<?php echo (new DateTime())->format('Y'); ?></p>
+        <p><strong>Team 18:</strong> William Baker, Simeon Dimitrov, William Masters, Eddie Page, Ross Massie, Daniel Christian-Lau</p>
+    </div>
 </footer>
